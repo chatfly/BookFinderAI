143c986db7919f3664135ccd8227b5d652c8eb1e
chatfly <oiamigopao@outlook.com>
2025-03-06 19:24:32 -0300

    feat: Adiciona os arquivos base para um projeto SpringBoot

---
+++ src/main/java/com/boofinder_ai/BookFinderAiApplication.java
@@ -0,0 +1,13 @@
+package com.boofinder_ai;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookFinderAiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BookFinderAiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/boofinder_ai/BookFinderAiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.boofinder_ai;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookFinderAiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
